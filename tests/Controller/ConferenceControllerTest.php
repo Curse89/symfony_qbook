cdf25a841094079ab3f2df9f8960e3a4e9f202c7
Сергей <curse1010@gmail.com>
2021-10-20 21:16:31 +0300

    Добавляю новое поле state к сущности Comment + тесты

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
@@ -41,10 +43,15 @@ class ConferenceControllerTest extends WebTestCase
         $client->submitForm('Submit', [
             'comment_form[author]' => 'Sergey',
             'comment_form[text]' => 'Коммент из теста',
-            'comment_form[email]' => 's@test.ru',
+            'comment_form[email]' => $email = 's@test.ru',
             'comment_form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
         ]);
         $this->assertResponseRedirects();
+
+		$comment = self::$container->get(CommentRepository::class)->findOneByEmail($email);
+		$comment->setState('published');
+		self::$container->get(EntityManagerInterface::class)->flush();
+
         $client->followRedirect();
         $this->assertSelectorExists('div:contains("Здесь 2 комментариев")');
 
