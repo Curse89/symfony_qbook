ac9a858330de80d85f7ce80e840ff3ab10e119f5
Curse1010 <curse1010@gmail.com>
2021-10-20 15:59:50 +0300

    Добавляю тест конференции путем отправки запроса на /

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback!');
+        echo $client->getResponse();
+    }
+}
