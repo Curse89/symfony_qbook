3a6a5cc0e0ed11dc089f4e1b7ebfe06fa6205fbd
Сергей <curse1010@gmail.com>
2021-10-21 22:00:44 +0300

    Отправляю письмо в обработчике сообщений

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -8,6 +8,8 @@ use App\SpamChecker;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -19,19 +21,28 @@ class CommentMessageHandler implements MessageHandlerInterface
 	private $commentRepository;
 	private $bus;
 	private $workflow;
-//	private $notifier;
-//	private $imageOptimizer;
-//	private $photoDir;
+	private $mailer;
+	private $adminEmail;
 	private $logger;
 
-	public function __construct(EntityManagerInterface $entityManager, SpamChecker $spamChecker, CommentRepository $commentRepository, MessageBusInterface $bus, WorkflowInterface $commentStateMachine, LoggerInterface $logger = null)
-	{
+	public function __construct(
+		EntityManagerInterface $entityManager,
+		SpamChecker $spamChecker,
+		CommentRepository $commentRepository,
+		MessageBusInterface $bus,
+		WorkflowInterface $commentStateMachine,
+		MailerInterface $mailer,
+		string $adminEmail,
+		LoggerInterface $logger = null
+	) {
 		$this->entityManager = $entityManager;
 		$this->spamChecker = $spamChecker;
 		$this->commentRepository = $commentRepository;
-        $this->bus = $bus;
-        $this->workflow = $commentStateMachine;
-        $this->logger = $logger;
+		$this->bus = $bus;
+		$this->workflow = $commentStateMachine;
+		$this->mailer = $mailer;
+		$this->adminEmail = $adminEmail;
+		$this->logger = $logger;
 	}
 
 	public function __invoke(CommentMessage $message)
@@ -42,22 +53,30 @@ class CommentMessageHandler implements MessageHandlerInterface
 		}
 
 		if ($this->workflow->can($comment, 'accept')) {
-            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
-            $transition = 'accept';
-            if (2 === $score) {
-                $transition = 'reject_spam';
-            } elseif (1 === $score) {
-                $transition = 'might_be_spam';
-            }
-            $this->workflow->apply($comment, $transition);
-            $this->entityManager->flush();
+			$score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+			$transition = 'accept';
+			if (2 === $score) {
+				$transition = 'reject_spam';
+			} elseif (1 === $score) {
+				$transition = 'might_be_spam';
+			}
+			$this->workflow->apply($comment, $transition);
+			$this->entityManager->flush();
 
-            $this->bus->dispatch($message);
-        } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
-            $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
-            $this->entityManager->flush();
-        } elseif ($this->logger) {
-            $this->logger->debug('Брошенный коммент сообщение', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
-        }
+			$this->bus->dispatch($message);
+		} elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
+			$this->mailer->send((new NotificationEmail())
+				->subject('Новый комментарий отправлен')
+				->htmlTemplate('emails/comment_notification.html.twig')
+				->from($this->adminEmail)
+				->to($this->adminEmail)
+				->context(['comment' => $comment])
+			);
+		} elseif ($this->logger) {
+			$this->logger->debug(
+				'Брошенный коммент сообщение',
+				['comment' => $comment->getId(), 'state' => $comment->getState()]
+			);
+		}
 	}
 }
