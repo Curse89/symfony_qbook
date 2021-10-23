c2701eeb023718c25b6ef7f4b1febcc3df33615c
Сергей <curse1010@gmail.com>
2021-10-23 15:17:32 +0300

    Исправляю ошибки

--- src/Notification/CommentReviewNotification.php
+++ src/Notification/CommentReviewNotification.php
@@ -4,9 +4,11 @@ namespace App\Notification;
 
 use App\Entity\Comment;
 use Symfony\Component\Notifier\Message\EmailMessage;
+use Symfony\Component\Notifier\Notification\EmailNotificationInterface;
+use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\Recipient\EmailRecipientInterface;
 
-class CommentReviewNotification
+class CommentReviewNotification extends Notification implements EmailNotificationInterface
 {
 	private $comment;
 
