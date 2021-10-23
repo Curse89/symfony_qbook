501338d884bcde5150da96bdf6a090658c456f09
Сергей <curse1010@gmail.com>
2021-10-23 14:54:16 +0300

    25.1 Уведомление пользователей о проверке комментариев в браузере

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -14,6 +14,8 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -59,7 +61,7 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{slug}", name="conference")
      */
-    public function show(ConferenceRepository $conferenceRepository, Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
+    public function show(ConferenceRepository $conferenceRepository, Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir, NotifierInterface $notifier): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -89,8 +91,14 @@ class ConferenceController extends AbstractController
 
 			$this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
+			$notifier->send(new Notification('Спасибо за отзыв! Комментарий будет опубликован после проверки.', ['browser']));
+
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
+
+		if ($form->isSubmitted()) {
+			$notifier->send(new Notification('Вы можете проверить свою заявку? С этим есть проблемы.', ['browser']));
+		}
 
 		$offset = max(0, $request->query->getInt('offset', 0));
 		$paginator = $commentRepository->getCommentPaginator($conference, $offset);
